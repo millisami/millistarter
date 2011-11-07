77d732f4d34c36bad2f9a24135e0def2eec28d43
MillisamiSprout <sachin@sprout-technology.com>
2011-11-07 14:11:51 +0545

    initial commit with gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Millistarter::Application.config.secret_token = '25f7a5917526232cf2a0cf9f9228e42ae941651403603e8e09d9572c6acf57598dcc703840ff5873df6c99ce2bf4aa4f86e05b084929a36c8a106f2bcc4651d3'
